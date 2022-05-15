9214801314bb3f6337fa80063ef18764afb372da
Anay Dilip Naik <89814957+anaydilipnaik@users.noreply.github.com>
2022-05-14 22:43:39 -0700

    flow fixed for landing page

--- frontend/my-app/src/pages/Home.jsx
+++ frontend/my-app/src/pages/Home.jsx
@@ -1,34 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 import Services from '../components/Services';
-import Promo from '../components/Promo';
 import FeaturedRooms from '../components/FeaturedRooms';
-import DatePicker from "react-datepicker";
-import { RoomContext } from '../context';
-
-import moment from 'moment';
+import { getAllRooms } from '../controllers/rooms';
 
 export default function Home() {
+    const [featuredRooms, setFeaturedRooms] = useState(null);
+
+    useEffect(() => {
+        getAllRooms().then(res => {
+            setFeaturedRooms(res.data.data.slice(0, 3));
+        }).catch(err => console.log(err))
+    },[]);
+
     return (
         <>
-        
-        <Hero hero="defaultHero">
-            
-        </Hero>
-        
-        <Banner title="The best hotels around the world" subtitle="We have a wide range of hotel rooms you can select from">   
-        <Link to="/rooms" className="btn btn-primary">
-     Search 
-     </Link>
-       
-        </Banner>
-        <FeaturedRooms/>
-        <Promo/>
-        <Services/> 
-        
+            <Hero hero="defaultHero" />
+            <Banner title="The best hotels around the world" subtitle="We have a wide range of hotel rooms you can select from">   
+            <Link to="/rooms" className="btn btn-primary">
+                Search 
+            </Link>
+            </Banner>
+            <FeaturedRooms rooms={featuredRooms} />
+            <Services/> 
         </>
-
     )
 }

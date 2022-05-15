8451690514d30b64af8bf45212a71553bc456ce9
aishwarya95698 <67372169+aishwarya95698@users.noreply.github.com>
2022-05-15 00:10:30 -0700

    Add files via upload

---
+++ frontend/my-app/src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Form1 from './Form1';
+import FormSignup from './FormSignup'
+import FormSuccess from './FormSuccess'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+import RoomsContainer from '../components/RoomsContainer';
+import FormLogin from '../pages/FormLogin'
+
+function Login() {
+
+   
+    
+    return <Form1/>;
+    
+}
+export default Login;

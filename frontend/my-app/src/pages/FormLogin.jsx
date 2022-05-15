bd4e44bc5a61a7f1e07d55a202a9712334291d04
aishwarya95698 <67372169+aishwarya95698@users.noreply.github.com>
2022-05-15 00:05:26 -0700

    These two files are for 2 separate pages now

--- frontend/my-app/src/pages/FormLogin.jsx
+++ frontend/my-app/src/pages/FormLogin.jsx
@@ -2,17 +2,11 @@ import React from 'react';
 import validate from './validateInfo';
 import useForm from './useForm';
 import './Form.css';
-
-
-//import About from './pages/About';
-//import Footer from './components/Footer';
-//import Contact from './pages/Contact';
-//import Booknow from './pages/Booknow';
-
-
 import { Link } from 'react-router-dom';
+//import Login from './pages/Login';
+//import FormLogin from './pages/FormLogin';
 
-const FormSignup = ({ submitForm }) => {
+const FormLogin = ({ submitForm }) => {
   const { handleChange, handleSubmit, values, errors } = useForm(
     submitForm,
     validate
@@ -24,18 +18,7 @@ const FormSignup = ({ submitForm }) => {
         <h1>
           Creat an account today to get a $50 credit on all the hotels from EXOTICA!
         </h1>
-        <div className='form-inputs'>
-          <label className='form-label'>Username</label>
-          <input
-            className='form-input'
-            type='text'
-            name='username'
-            placeholder='Enter your username'
-            value={values.username}
-            onChange={handleChange}
-          />
-          {errors.username && <p>{errors.username}</p>}
-        </div>
+        
         <div className='form-inputs'>
           <label className='form-label'>Email</label>
           <input
@@ -60,27 +43,16 @@ const FormSignup = ({ submitForm }) => {
           />
           {errors.password && <p>{errors.password}</p>}
         </div>
-        <div className='form-inputs'>
-          <label className='form-label'>Confirm Password</label>
-          <input
-            className='form-input'
-            type='password'
-            name='password2'
-            placeholder='Confirm your password'
-            value={values.password2}
-            onChange={handleChange}
-          />
-          {errors.password2 && <p>{errors.password2}</p>}
-        </div>
+        
         <button className='form-input-btn' type='submit'>
-          Sign up
+          Login
         </button>
         <span className='form-input-login'>
-          Already have an account? Login <Link to="/Login">here</Link>
+          Don't have an account? Signup <Link to="/Signup">here</Link>
         </span>
       </form>
     </div>
   );
 };
 
-export default FormSignup;
+export default FormLogin;
